78bef59ca4c8c003a97d36473f616dc3bb823b13
jacklam200 <jacklam200@gmail.com>
2022-10-22 11:14:53 +0800

    js函数定义及数据刷新，及wxml按钮交互

--- pages/log/index.js
+++ pages/log/index.js
@@ -4,5 +4,8 @@ Page({
     message: 'Hello MINA!',
     array: [1, 2, 3, 4, 5],
     staffA: {firstName: 'Hulk', lastName: 'Hu'}
+  },
+  clickMe: function() {
+    this.setData({ message: "Hello World" })
   }
 })
