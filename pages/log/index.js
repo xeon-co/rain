dd1424b5fcd747d7a58604e234f59f2e040aa83b
jacklam200 <jacklam200@gmail.com>
2022-10-23 17:12:49 +0800

    手动启动更新

--- pages/log/index.js
+++ pages/log/index.js
@@ -85,7 +85,7 @@ Page({
 
   data: {
     message: 'Hello MINA!',
-    array: [1, 2, 3, 4, 5],
+    array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     loading: true,
     staffA: {firstName: 'Hulk', lastName: 'Hu'}
   },
@@ -105,6 +105,33 @@ Page({
     this.clearAnimation('#tapTest', { opacity: true, rotate: true, backgroundColor:'#FF0000'}, function () {
       console.log("清除了#tapTest opacity 和rotate属性")
      
+    })
+  },
+  tapName4: function(event) {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res)
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+      console.log("onUpdateFailed")
     })
   },
   tapName1: function(event) {
