94e77bef05605e0297830a5344f1b829ff19e900
jacklam200 <jacklam200@gmail.com>
2022-10-23 17:43:13 +0800

    监控数据变化

--- pages/log/component.js
+++ pages/log/component.js
@@ -1,4 +1,14 @@
+var myBehavior = require('./behaviors.js')
 Component({
+  behaviors: [myBehavior],
+  observers: {
+    'numberA, numberB': function(numberA, numberB) {
+      // 在 numberA 或者 numberB 被设置时，执行这个函数
+      this.setData({
+        sum: numberA + numberB
+      })
+    }
+  },
   properties: {
     // 这里定义了 innerText 属性，属性值可以在组件使用时指定
     innerText: {
@@ -10,9 +20,38 @@ Component({
     multipleSlots: true // 在组件定义时的选项中启用多 slot 支持
   },
   data: {
-    text: "This is page data."
+    text: "page data.",
+    numberA: 1,
+    numberB: 2,
+    sum:0
+  },
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在 methods 段中定义的方法名
+    attached: function () { 
+      console.log("Component attached")
+    },
+    moved: function () { 
+      console.log("Component moved")
+    },
+    detached: function () { 
+      console.log("Component detached")
+    },
+  },
+  // 生命周期函数，可以为函数，或一个在 methods 段中定义的方法名
+  attached: function () { 
+    console.log("Component attached1")
+  }, // 此处 attached 的声明会被 lifetimes 字段中的声明覆盖
+  ready: function() { 
+    console.log("Component ready")
   },
   pageLifetimes: {
+    // 组件所在页面的生命周期函数
+    show: function () { 
+      console.log("Component show")
+    },
+    hide: function () { 
+      console.log("Component hide")
+    },
     resize(res) {
       res.size.windowWidth // 新的显示区域宽度
       res.size.windowHeight // 新的显示区域高度
@@ -28,6 +67,10 @@ Component({
     // 事件响应函数
     viewTap: function() {
       // ...
+      this.setData({
+        numberA:0,
+        numberB:4
+      })
     }
   }
 })
