6ed6cd686b3cc0e2479c09216d9d74f840c66506
jacklam200 <jacklam200@gmail.com>
2022-10-23 17:26:22 +0800

    增加且使用component组件

--- pages/log/component.js
+++ pages/log/component.js
@@ -1,4 +1,11 @@
 Component({
+  properties: {
+    // 这里定义了 innerText 属性，属性值可以在组件使用时指定
+    innerText: {
+      type: String,
+      value: 'default value',
+    }
+  },
   data: {
     text: "This is page data."
   },
